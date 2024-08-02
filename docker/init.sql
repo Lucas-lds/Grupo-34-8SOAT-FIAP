60a513bb628016a2bf3711a2679640d8c9e4fc6e
Jean Gabriel <jeangabriel180@hotmail.com>
2024-08-01 22:03:20 -0300

    adicao campo cpf

--- docker/init.sql
+++ docker/init.sql
@@ -3,7 +3,7 @@ CREATE TABLE tb_clientes (
     nome VARCHAR(100) NOT NULL,                -- Nome do cliente
     email VARCHAR(100) UNIQUE NOT NULL,        -- Email do cliente
     telefone VARCHAR(15),                      -- Telefone do cliente
-    cpf VARCHAR(14),
+    cpf VARCHAR(14),                           -- cpf do cliente
     data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP  -- Data e hora de cadastro
 );
 
