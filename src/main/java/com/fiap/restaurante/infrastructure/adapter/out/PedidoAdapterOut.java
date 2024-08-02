aa26c2eaa94fc5796d3db27d5da3032c182bdcc1
trancoso97 <trancosorodrigo1@gmail.com>
2024-08-02 19:57:27 -0300

    Fixing create order operation

--- src/main/java/com/fiap/restaurante/infrastructure/adapter/out/PedidoAdapterOut.java
+++ src/main/java/com/fiap/restaurante/infrastructure/adapter/out/PedidoAdapterOut.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.fiap.restaurante.application.port.out.PedidoAdapterPortOut;
@@ -13,22 +14,27 @@ import com.fiap.restaurante.core.domain.Pedido;
 import com.fiap.restaurante.infrastructure.adapter.out.entity.ClienteEntity;
 import com.fiap.restaurante.infrastructure.adapter.out.entity.PedidoEntity;
 import com.fiap.restaurante.infrastructure.adapter.out.entity.PedidoProdutoEntity;
+import com.fiap.restaurante.infrastructure.adapter.out.entity.PedidoProdutoPK;
 import com.fiap.restaurante.infrastructure.adapter.out.entity.ProdutoEntity;
+import com.fiap.restaurante.infrastructure.adapter.out.repository.ClienteRepository;
 import com.fiap.restaurante.infrastructure.adapter.out.repository.PedidoProdutoRepository;
 import com.fiap.restaurante.infrastructure.adapter.out.repository.PedidoRepository;
+import com.fiap.restaurante.infrastructure.adapter.out.repository.ProdutoRepository;
 
 @Component
 public class PedidoAdapterOut implements PedidoAdapterPortOut {
 
-    private final PedidoRepository pedidoRepository;
-    private final PedidoProdutoRepository pedidoProdutoRepository;
-    private final ModelMapper mapper;
+    @Autowired
+    private PedidoRepository pedidoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
+    @Autowired
+    private PedidoProdutoRepository pedidoProdutoRepository;
+    @Autowired
+    private ModelMapper mapper;
 
-    public PedidoAdapterOut( PedidoRepository pedidoRepository, PedidoProdutoRepository pedidoProdutoRepository, ModelMapper mapper) {
-        this.pedidoRepository = pedidoRepository;
-        this.pedidoProdutoRepository = pedidoProdutoRepository;
-        this.mapper = mapper;
-    }
 
     @Override
     public Pedido atualizarStatusPedido(OrderStatus status, Long id) {
@@ -44,22 +50,26 @@ public class PedidoAdapterOut implements PedidoAdapterPortOut {
     public Pedido criarPedido(Pedido pedido) {
         var pedidoEntity = new PedidoEntity();
         pedidoEntity.setStatus_(pedido.getStatus());
-        pedidoEntity.setCliente(ClienteEntity.fromDomain(new Cliente(pedido.getIdCliente())));
-        var pedidoProdutos = new HashSet<PedidoProdutoEntity>();
+        pedidoEntity.setIdCliente(pedido.getIdCliente());
+        var pedidoSaved = this.pedidoRepository.save(pedidoEntity);
         pedido.getListaPedidoProdutos().forEach((pedidoProduto) -> {
             var entity = new PedidoProdutoEntity();
-            entity.setPedido(pedidoEntity);
-            entity.setProduto(ProdutoEntity.fromDomain(pedidoProduto.getProduto()));
+            entity.setPedido(pedidoSaved);
+            var produto = this.produtoRepository.findById(pedidoProduto.getProduto().getIdProduto());
+            entity.setProduto(produto.get());
             entity.setQuantidade(pedidoProduto.getQuantidade());
-            pedidoProdutos.add(entity);
+            this.pedidoProdutoRepository.save(entity);
         });
-        pedidoEntity.setPedidoProdutos(pedidoProdutos);
+
         return mapper.map(pedidoRepository.save(pedidoEntity), Pedido.class);
     }
 
     @Override
     public Pedido listarPedidoPorId(Long id) {
-        return mapper.map(pedidoRepository.findById(id), Pedido.class);
+        if(pedidoRepository.findById(id).isPresent()){
+            var pedidoProduto = this.pedidoProdutoRepository.findById(id);
+        }
+        return new Pedido();
     }
 
     @Override
--- src/main/java/com/fiap/restaurante/infrastructure/adapter/out/entity/PedidoEntity.java
+++ src/main/java/com/fiap/restaurante/infrastructure/adapter/out/entity/PedidoEntity.java
@@ -11,9 +11,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -36,11 +33,6 @@ public class PedidoEntity {
     @Enumerated(EnumType.STRING)
     private OrderStatus status_;
 
-    @ManyToOne
-    @JoinColumn(name = "id_cliente")
-    private ClienteEntity cliente;
-    
-    @OneToMany(mappedBy = "pedido", orphanRemoval = true)
-    private Set<PedidoProdutoEntity> pedidoProdutos;
+    private Long idCliente;
 
 }
