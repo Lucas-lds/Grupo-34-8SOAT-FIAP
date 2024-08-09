00ed5c1b7643db22f7e42c642fe5d885fb91c0be
trancoso97 <trancosorodrigo1@gmail.com>
2024-08-09 00:44:32 -0300

    fix: fixing operação pagamento

--- src/main/java/com/fiap/restaurante/application/service/PagamentoService.java
+++ src/main/java/com/fiap/restaurante/application/service/PagamentoService.java
@@ -1,10 +1,12 @@
 package com.fiap.restaurante.application.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.fiap.restaurante.application.port.out.PagamentoAdapterPortOut;
 import com.fiap.restaurante.application.port.out.PagamentoServicePortOut;
 
+@Service
 public class PagamentoService implements PagamentoServicePortOut{
 
     @Autowired
--- src/main/java/com/fiap/restaurante/core/usecase/PagamentoUseCase.java
+++ src/main/java/com/fiap/restaurante/core/usecase/PagamentoUseCase.java
@@ -1,10 +1,12 @@
 package com.fiap.restaurante.core.usecase;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.fiap.restaurante.application.port.out.PagamentoServicePortOut;
 import com.fiap.restaurante.application.port.out.usecase.PagamentoUseCasePortOut;
 
+@Component
 public class PagamentoUseCase implements PagamentoUseCasePortOut{
 
     @Autowired
