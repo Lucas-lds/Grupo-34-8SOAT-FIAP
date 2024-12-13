15da46dab68f5547e1013de2a231022106609337
Lucas-lds <49286712+Lucas-lds@users.noreply.github.com>
2024-12-12 22:07:01 -0300

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "gateway_url" {
+  value = aws_api_gateway_rest_api.my_api.url  # Exemplo de URL do Gateway API
+  description = "URL do API Gateway"
+}
+
+output "cognito_user_pool_id" {
+  value = aws_cognito_user_pool.my_user_pool.id  # ID do Cognito User Pool
+  description = "ID do Cognito User Pool"
+}
