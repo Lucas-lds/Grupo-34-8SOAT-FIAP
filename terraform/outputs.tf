25eedafcec9413d5e2b94ece8c2ee2d9ff1dc5c7
Lucas-lds <49286712+Lucas-lds@users.noreply.github.com>
2024-12-12 22:22:06 -0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "gateway_url" {
-  value = aws_api_gateway_rest_api.my_api.url  # Exemplo de URL do Gateway API
+  value = aws_api_gateway_rest_api.auth_api.execution_arn  # Exemplo de URL do Gateway API
   description = "URL do API Gateway"
 }
 
 output "cognito_user_pool_id" {
-  value = aws_cognito_user_pool.my_user_pool.id  # ID do Cognito User Pool
+  value = aws_cognito_user_pool.restaurante_user_pool.id  # ID do Cognito User Pool
   description = "ID do Cognito User Pool"
 }
