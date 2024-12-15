1eafb53297d985f010235358def1bba43dc94641
Lucas-lds <49286712+Lucas-lds@users.noreply.github.com>
2024-12-14 22:45:25 -0300

    Update k8s.tf

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -31,7 +31,7 @@ resource "kubernetes_deployment" "restaurante-api" {
 
         container {
           name  = "restaurante"
-          image = "717279688908.dkr.ecr.us-east-1.amazonaws.com/repositorio:v5"
+          image = "717279688908.dkr.ecr.us-east-1.amazonaws.com/repositorio:v9"
 
           env {
             name  = "RDS_ENDPOINT"
