c353eadaeeb9cd749208d6b829dd2cf9af63aeef
rafa-fortes <r.s.neves@hotmail.com>
2025-01-29 22:13:44 -0300

    Refatoração da fundação: IAC para microsserviços, repositórios e integração de bancos

--- docker/build_and_push.sh
+++ docker/build_and_push.sh
@@ -12,6 +12,14 @@ IMAGE_TAG="v8"  # Defina a tag da imagem aqui
 # Diretório de contexto é o diretório pai onde o código fonte está localizado
 CONTEXT_DIR=".."  # Um nível acima, onde está o código fonte e outros arquivos
 
+# Carregar variáveis de ambiente do arquivo .env
+if [ -f ".env" ]; then
+    export $(cat .env | xargs)
+else
+    echo ".env file not found. Please create the file with the necessary environment variables."
+    exit 1
+fi
+
 # Certifique-se de que o Dockerfile existe
 if [ ! -f "Dockerfile" ]; then
     echo "Dockerfile not found. Please check the path."
@@ -19,7 +27,8 @@ if [ ! -f "Dockerfile" ]; then
 fi
 
 # Build Docker image
-docker build -t $REPO_NAME:$IMAGE_TAG -f Dockerfile $CONTEXT_DIR || { echo "Docker build failed"; exit 1; }
+#docker build -t $REPO_NAME:$IMAGE_TAG -f Dockerfile $CONTEXT_DIR || { echo "Docker build failed"; exit 1; }
+docker build --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -t $REPO_NAME:$IMAGE_TAG -f Dockerfile $CONTEXT_DIR || { echo "Docker build failed"; exit 1; }
 
 # Tag Docker image
 docker tag $REPO_NAME:$IMAGE_TAG $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG || { echo "Docker tag failed"; exit 1; }
@@ -32,5 +41,3 @@ echo $AWS_LOGIN_PASSWORD | docker login --username AWS --password-stdin $ACCOUNT
 docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPO_NAME:$IMAGE_TAG || { echo "Docker push failed"; exit 1; }
 
 echo "Docker image successfully pushed to ECR."
-
-
